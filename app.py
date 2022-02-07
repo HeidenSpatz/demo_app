a06de6a8539800c0de1b4779e755cdff32937177
Robert Uwe Litschel <uwelitschel@gmail.com>
2022-02-07 14:20:48 +0100

    df added

--- app.py
+++ app.py
@@ -16,27 +16,31 @@ st.set_page_config(page_title='my app',
 
 st.title("a simple demo app")
 
+df = pd.DataFrame()
+
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
 
     # todo: if not csv
 
-     # Can be used wherever a "file-like" object is accepted:
-     df = pd.read_csv(uploaded_file)
+    # Can be used wherever a "file-like" object is accepted:
+    df = pd.read_csv(uploaded_file)
+
+    #data overview
+    st.header("Overview")
+    st.write("rows: ", df.shape[0], "cols: ", df.shape[1])
+    st.write(df.columns)
 
 
-#data overview
-st.header("Overview")
-st.write("rows: ", df.shape[0], "cols: ", df.shape[1])
-st.write(df.columns)
+    #show available columns
+    st.subheader("Column names")
+    column_names = ""
+    for column in pd.Series(df.columns):
+        column_names = column_names + ", " + column
+
+    st.write(column_names)
 
 
-#show available columns
-st.subheader("Column names")
-column_names = ""
-for column in pd.Series(df.columns):
-    column_names = column_names + ", " + column
 
-st.write(column_names)
 
