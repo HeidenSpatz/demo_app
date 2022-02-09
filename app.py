6ad08c153cd314414216f8d114d9390623989c53
Robert Uwe Litschel <uwelitschel@gmail.com>
2022-02-09 15:36:47 +0100

    syipy

--- app.py
+++ app.py
@@ -65,50 +65,17 @@ col_selected = st.selectbox("Select Column", col_names, 0)
 #------------------
 # chart
     
-import plotly.figure_factory as ff
+import plotly.graph_objects as go
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
+#X = df[col_selected].nlargest(10)
+#st.write(X)
 
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-         hist_data, group_labels, bin_size=[.1, .25, .5])
-
+fig = go.Figure(go.Bar(
+            x=[20, 14, 23],
+            y=['giraffes', 'orangutans', 'monkeys'],
+            marker_color='skyblue',
+            orientation='h'))
 st.plotly_chart(fig, use_container_width=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import plotly.graph_objects as go
-
-
-# fig = go.Figure(go.Bar(
-#             x=[20, 14, 23],
-#             y=['giraffes', 'orangutans', 'monkeys'],
-#             marker_color='skyblue',
-#             orientation='h'))
-# st.plotly_chart(fig, use_container_width=True)
 
 # fig2 = go.Figure(go.Bar(
 #             x=coeff['Coefficient'],
